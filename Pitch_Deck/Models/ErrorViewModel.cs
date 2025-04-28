d675e319436d380094cd96ba583bb2b0471b509c
izzat amir <a189571@siswa.ukm.edu.my>
2025-04-28 23:48:45 +0800

    Add project files.

---
+++ Pitch_Deck/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pitch_Deck.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
